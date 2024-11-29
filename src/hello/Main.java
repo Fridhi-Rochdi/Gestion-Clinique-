e3a4fe35633ff3ceefc8b68c328973bceb8c9b82
sarah20002 <sarra.laouini@enicar.ucar.tn>
2024-11-29 20:44:10 +0100

    commit

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,5 +1,6 @@
 package hello;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,14 +8,19 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/hello/Welcome.fxml")); // Chemin vers Welcome.fxml
+public void start(Stage primaryStage) {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/hello/FactureView.fxml"));
         primaryStage.setScene(new Scene(loader.load()));
         primaryStage.setTitle("Gestion Clinique");
         primaryStage.show();
+    } catch (IOException e) {
+        e.printStackTrace(); // Affiche l'exception complète
     }
+}
 
     public static void main(String[] args) {
+        // Lance l'application JavaFX
         launch(args);
     }
 }
